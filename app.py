70c17a5e9f959493716e6015d1be62399f3f5bc8
Tandon1 <117367384+Tandon1@users.noreply.github.com>
2025-06-28 19:33:48 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def index():
     return render_template('index.html', g_result=g_result, f_result=f_result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
